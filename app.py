25cf6b8d9e29a75f0c195b88df874e72f7e11caa
himwroteCode <106196673+himwroteCode@users.noreply.github.com>
2023-03-13 16:22:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+def home():
+    return render_template("home page.html")
+
+@app.route('/')
+def index():
+    return render_template("form.html")
+    
+
+@app.route('/uppercase')
+def uppercase():
+    strings_param = request.args.get('strings')
+    
+    strings = strings_param.split(',')
+    upper_strings = [s.strip().upper() for s in strings]
+    
+    message = "The uppercase strings are: " + ", ".join(upper_strings)
+    
+    return message
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
